87ea25e4445900153011ceabb51f0e415a515bd1
faresmo-web <sm3305358@gmail.com>
2025-02-01 02:29:37 +0200

    Online store

--- js/main.js
+++ js/main.js
@@ -2,3 +2,21 @@ let categoryNavList = document.querySelector(".category-nav-list")
 function openCategList(){
     categoryNavList.classList.toggle("active")
 }
+
+
+
+
+
+
+
+var swiper = new Swiper(".slide-swp", {
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullests: true,
+        clickable:true
+    },
+    autoplay:{
+        delay:2500
+    }
+    ,loop:true
+});
