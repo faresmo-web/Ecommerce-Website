cd49ffdd6b6e401b9900ac195ac6d8e5b82ba76d
faresmo-web <sm3305358@gmail.com>
2025-02-01 19:33:50 +0200

    Online store

--- js/main.js
+++ js/main.js
@@ -9,14 +9,3 @@ function openCategList(){
 
 
 
-var swiper = new Swiper(".slide-swp", {
-    pagination: {
-        el: ".swiper-pagination",
-        dynamicBullests: true,
-        clickable:true
-    },
-    autoplay:{
-        delay:2500
-    }
-    ,loop:true
-});
---
+++ js/swiper.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper(".slide-swp", {
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullests: true,
+        clickable:true
+    },
+    autoplay:{
+        delay:2500
+    }
+    ,loop:true
+});
+
+
+// SWIPR SLIDE PORDUCTS
+
+var swiper = new Swiper(".slide-product", {
+    slidesPerView: 5,
+    spaceBetween:20,
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullests: true,
+        clickable:true
+    },
+    autoplay:{
+        delay:2500
+    }
+    ,loop:true
+});
